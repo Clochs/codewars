148e6922b65d900eb726a2cc9d7ff716f3e075a2
Sam Ryan <111122797+Clochs@users.noreply.github.com>
2024-02-04 14:22:22 -0600

    Twice as old

---
+++ javascript/8kyu/twice-as-old.js
@@ -0,0 +1,20 @@
+// Your function takes two arguments:
+
+// current father's age (years)
+// current age of his son (years)
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
+
+// My solution
+
+
+function twiceAsOld(dadYearsOld, sonYearsOld){
+    if(sonYearsOld * 2 > dadYearsOld){
+        return sonYearsOld * 2 - dadYearsOld
+    }
+    return dadYearsOld - sonYearsOld * 2
+}
+
+// Better solution
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    return Math.abs(dadYearsOld - 2 * sonYearsOld);
+  }
