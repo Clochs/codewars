d8ebafc4df3077e32ec00e8bf352a314b1f2e292
Sam Ryan <111122797+Clochs@users.noreply.github.com>
2024-02-03 23:24:00 -0600

    Third Angle of a Triangle

---
+++ third-angle-of-a-triangle.js
@@ -0,0 +1,9 @@
+// You are given two interior angles (in degrees) of a triangle.
+
+// Write a function to return the 3rd.
+
+// My solution
+// Subtract the sum of a + b from 180 degrees
+function otherAngle(a, b) {
+    return 180 - a - b;
+  }
