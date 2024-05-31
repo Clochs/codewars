524fef0ae9a0def584f5d8ec55befc38b40bb4c6
Clochs <sam_vs_computer@gmail.com>
2024-05-30 22:55:15 -0500

    restored all files

--- javascript/remove-exclamation-marks.js
+++
@@ -1,7 +0,0 @@
-// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
-
-// My solution
-
-function RemoveExclamationMarks(s){
-    return s.replaceAll('!', '')
-}
--- javascript/replace-all-vowels-with-exclamation-mark.js
+++
@@ -1,8 +0,0 @@
-// Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
-
-// Solution
-
-function replaceVowels(str){
-    return str.replace(/[aeiou]/ig, '!')
-}
-
