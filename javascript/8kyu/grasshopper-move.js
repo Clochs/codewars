576c648107837903db8bd9aa316344873a8e0fd7
Clochs <sam_vs_computer@gmail.com>
2024-08-03 07:49:07 -0500

    Grasshopper terminal game move function

---
+++ javascript/8kyu/grasshopper-move.js
@@ -0,0 +1,7 @@
+// Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+
+// My solution
+
+function move (position, roll) {
+    return (roll * 2) + position
+  }
