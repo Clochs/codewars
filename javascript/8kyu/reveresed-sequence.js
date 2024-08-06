c50fbbe0da1f834f3e7e645718d8b609f2a4e6bc
Clochs <sam_vs_computer@gmail.com>
2024-08-06 13:33:05 -0500

    Revered sequence

---
+++ javascript/8kyu/reveresed-sequence.js
@@ -0,0 +1,11 @@
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// My solution
+
+const reverseSeq = n => {
+    newArr = []
+    for (let i = n; i > 0; i--) {
+      newArr.push(i)
+    }
+    return newArr
+  };
