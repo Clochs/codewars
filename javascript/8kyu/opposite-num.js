94eef5d66309a3631a1bba793e8099f710988ff8
Clochs <aloewishus83@gmail.com>
2024-09-05 08:32:45 -0500

    Return the number opposite

---
+++ javascript/8kyu/opposite-num.js
@@ -0,0 +1,8 @@
+// Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+
+// My solution
+
+function opposite(number) {
+    return -number
+  }
