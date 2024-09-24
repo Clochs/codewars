5e0ea0da097e4ac1c43bd502a5fc40db9fdb4459
Clochs <aloewishus83@gmail.com>
2024-09-23 22:19:45 -0500

    forgot to save before last commit

--- javascript/8kyu/args-to-bin.js
+++ javascript/8kyu/args-to-bin.js
@@ -4,7 +4,8 @@
 // My solution
 
 function arr2bin(arr) {
-    // declare a new var to return at the end of the function
+    // Check if any element is NaN if so return NaN .some() runs callback function and returns a boolean
+    // Declare a new var to return at the end of the function
     // Use reduce to return a single sum (total)
     // Check if num is an integer and return sum
     // If not integer, return 0
@@ -15,11 +16,24 @@ function arr2bin(arr) {
     }
     
     let newArr = arr.reduce((total, num) => {
-        return Number.isInteger(num) ? total + num : 0;
+        return Number.isInteger(num) ? total + num : total;
     }, 0);
     return newArr.toString(2);
 };
 
-console.log(arr2bin([2, 4, 6]))
+console.log(arr2bin([2, 4, 6]));
+
+
+
+// Much more concise
+
+// Use reduce to return a sum
+// Only if item === number add item to sum otherwise ignore
+// Initiate to 0
+// Change sum to binary using toString(2) (base 2)
+
+function arr2bin(arr) {
+    return arr.reduce((sum, item) => typeof(item) === 'number' ? sum + item : sum, 0).toString(2);
+  }
 
 
