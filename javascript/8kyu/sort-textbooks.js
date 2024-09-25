e98aafa0949cd321a329d556c112b14e6b9f569b
Clochs <aloewishus83@gmail.com>
2024-09-25 10:19:45 -0500

    Added comments to explain code logic

--- javascript/8kyu/sort-textbooks.js
+++ javascript/8kyu/sort-textbooks.js
@@ -3,6 +3,14 @@
 
 // My solution
 
+
+// Sort return -1, 0 or 1 to determain sorting placement of element
+// IF a is greater than b 
+//    return 1 (swap a with b) because a should come after b
+// ELSEIF a is less than b 
+//    return - 1 (keep a before b) because a should come before b
+// ELSE
+//    return 0 because a and b are equal 
 function sorter(textbooks) {
     return textbooks.sort((a, b) => a.toLowerCase() > b.toLowerCase() ? 1 : 
                                     a.toLowerCase() < b.toLowerCase() ? -1  : 0)
