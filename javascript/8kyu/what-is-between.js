6d4b6599acfae72ed9db23138b325d551a2dd8ab
Clochs <aloewishus83@gmail.com>
2024-11-04 07:25:49 -0800

    What is between

---
+++ javascript/8kyu/what-is-between.js
@@ -0,0 +1,27 @@
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+// For example:
+
+// a = 1
+// b = 4
+// --> [1, 2, 3, 4]
+
+
+// My solution
+
+// Declare empty arr
+// Use a for loop start at a
+// Count from a to b
+// Print every iteration between 
+
+
+function between(a, b) {
+    let arr = []
+    for (let i = a; i <= b; i++) {
+        arr.push(i);
+    }
+    return arr;
+};
+
+
+console.log(between(1, 5));
