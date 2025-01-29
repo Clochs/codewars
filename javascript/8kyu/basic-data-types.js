d1c4b3011d70ef2b7dbf6ce552e0bf535b66ae8f
Clochs <aloewishus83@gmail.com>
2025-01-29 03:36:11 -0800

    forgot to save

--- javascript/8kyu/basic-data-types.js
+++ javascript/8kyu/basic-data-types.js
@@ -4,30 +4,33 @@
 // 4. pushElement(arr)  should push an element to arr, and then return arr
 // 5. popElement(arr)   should pop an element from arr, and then return arr
 
+
+// My solution
+
+
 function getLength(arr){
-    //return length of arr
-    return arr.length;
-  };
+  //return length of arr
+  return arr.length;
+};
 console.log(getLength("Sam"))
-  function getFirst(arr){
-    //return the first element of arr
-    return arr[0];
-  };
-  console.log(getFirst("Sam"));
-  function getLast(arr){
-    //return the last element of arr
-    return arr[arr.length - 1];
-  };
+function getFirst(arr){
+  //return the first element of arr
+  return arr[0];
+};
+console.log(getFirst("Sam"));
+function getLast(arr){
+  //return the last element of arr
+  return arr[arr.length - 1];
+};
 //   console.log(getLast("Sam"));
-  function pushElement(arr){
-    var el=1;
-    //push el to arr
-    arr.push(el);
-    return arr;
-  };
-  console.log(pushElement("5432"));
-  function popElement(arr){
-    //pop an element from arr
-    arr.pop()
-    return arr; 
-  }
+function pushElement(arr){
+  var el=1;
+  //push el to arr
+arr.push(el);
+  return arr;
+};
+function popElement(arr){
+  //pop an element from arr
+  arr.pop()
+  return arr
+}
